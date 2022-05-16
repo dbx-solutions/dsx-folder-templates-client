e2eacda2674d13416c5676d65bdd3da9f501bac6
Hussam Hebbo <hussam@dropbox.com>
2022-05-16 09:48:56 +0200

    updated frontend style for select

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Select from 'react-select'
 import styles from "./main.module.css";
 
 export default function Main() {
   const [folderName, setFolderName] = useState("");
+  const [templates, setTemplates] = useState([]);
+  const [selectedTemplate, setSelectedTemplate] = useState("");
+
+  useEffect(() => {
+    prepareTemplateNameItems()
+  }, []);
 
   function connectToDropbox() {
     fetch("/authurl")
@@ -13,11 +20,33 @@ export default function Main() {
   function createFromTemplate() {
     fetch("/template?" + new URLSearchParams({
       rootName: folderName,
+      templateName: selectedTemplate,
     }))
     .then(() => window.location.replace("/"))
   }
 
-   return (
+  function prepareTemplateNameItems() {
+    fetch("/template-list")
+    .then((res) => res.json())
+    .then((data) => setTemplates(data.templates))
+  }
+
+  function getSelectedTemplate(e) {
+    setSelectedTemplate(e.value)
+  }
+
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      width: "100%",
+      height: "5vh",
+      border: "none",
+      borderRadius: "0.5em",
+      backgroundColor: "transparent"
+    }),
+  }
+
+  return (
     <>
       <div className={styles.taglineContainer}>
         <h1 className={styles.tagline}>
@@ -30,12 +59,22 @@ export default function Main() {
 
         <div className={styles.horizontalLine} />
 
+        <Select 
+          options={templates} 
+          styles={customStyles}
+          className={styles.selectContainer}
+          isClearable={false}
+          onChange={getSelectedTemplate}
+          classNamePrefix="select"
+        />
+
         <input 
           className={styles.input} 
           placeholder="Enter root folder name"
           value={folderName} 
           onChange={e => setFolderName(e.target.value)}
         />
+
         <button className={styles.button} onClick={createFromTemplate}>Create from template</button>
       </div>
     </>
